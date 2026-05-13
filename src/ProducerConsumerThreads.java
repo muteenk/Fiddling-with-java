9dad56a92fb7b6a064952fc451c5df619c35e74e
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-05-13 23:30:32 +0530

    Fixed the producer consumer error

--- src/ProducerConsumerThreads.java
+++ src/ProducerConsumerThreads.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.Semaphore;
 
 class Task {
     private final String title;
@@ -18,13 +19,13 @@ class Task {
         System.out.println("STATUS: " + this.status);
         System.out.println("-----------------------\n");
     }
-
 }
 
 public class ProducerConsumerThreads {
-
+    private final Object mutex = new Object();
+    private final Semaphore emptySlots = new Semaphore(5);
+    private final Semaphore occupiedSlots = new Semaphore(0);
     private final Queue<Task> taskQueue = new LinkedList<>();
-    private final int MAX_CAP = 5;
 
     class Producer implements Runnable {
         private final Task task;
@@ -32,15 +33,30 @@ public class ProducerConsumerThreads {
             this.task = task;
         }
         public void run() {
-            if (taskQueue.size() < MAX_CAP)
-                taskQueue.add(task);
+            try {
+                emptySlots.acquire();
+                synchronized (mutex) {
+                    taskQueue.add(task);
+                }
+                occupiedSlots.release();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
     class Consumer implements Runnable {
         public void run() {
-            Task t = taskQueue.remove();
-            t.markDone();
+            try {
+                occupiedSlots.acquire();
+                synchronized (mutex) {
+                    Task t = taskQueue.remove();
+                    t.markDone();
+                }
+                emptySlots.release();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
@@ -57,26 +73,24 @@ public class ProducerConsumerThreads {
             consumers.add(new Thread(pc.new Consumer()));
         }
 
-        for (int i = 0; i < 20; i++){
+        for (int i = 0; i < 40; i++){
             producers.get(i).start();
             consumers.get(i).start();
         }
 
-//        for (int i = 0; i < 20; i++){
-//            try {
-//                producers.get(i).join();
-//                consumers.get(i).join();
-//            } catch (Exception e){
-//                e.printStackTrace();
-//                producers.get(i).interrupt();
-//                consumers.get(i).interrupt();
-//            }
-//        }
+        for (int i = 0; i < 40; i++){
+            try {
+                producers.get(i).join();
+                consumers.get(i).join();
+            } catch (Exception e){
+                e.printStackTrace();
+                producers.get(i).interrupt();
+                consumers.get(i).interrupt();
+            }
+        }
 
         for (Task task: allTasks){
             task.repr();
         }
-
     }
-
 }
